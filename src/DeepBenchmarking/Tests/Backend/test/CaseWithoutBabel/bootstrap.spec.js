e30fbe0321e40c8b51890f8a89dee0c97cad04c7
vcernomschi <vcernomschi@mitocgroup.com>
2016-05-03 15:34:27 +0300

    Update gitignore

--- src/DeepBenchmarking/Tests/Backend/test/CaseWithoutBabel/bootstrap.spec.js
+++ src/DeepBenchmarking/Tests/Backend/test/CaseWithoutBabel/bootstrap.spec.js
@@ -1,15 +1,13 @@
-// THIS TEST WAS GENERATED AUTOMATICALLY ON Tue May 03 2016 15:09:24 GMT+0300 (EEST)
+// THIS TEST WAS GENERATED AUTOMATICALLY ON Tue May 03 2016 15:31:04 GMT+0300 (EEST)
 
 'use strict';
 
 import chai from 'chai';
 import bootstrap from '../../node_modules/CaseWithoutBabel/bootstrap';
 
-let expect = chai.expect;
-
 // @todo: Add more advanced tests
 suite('Bootstraps', () => {
   test(' bootstrap exists in without-babel module', () => {
-    expect(bootstrap).to.be.an('object');
+    chai.expect(bootstrap).to.be.an('object');
   });
 });
--- src/DeepBenchmarking/Tests/Backend/test/LambdaSizeRetrieve/Handler.spec.js
+++ src/DeepBenchmarking/Tests/Backend/test/LambdaSizeRetrieve/Handler.spec.js
@@ -1,15 +1,13 @@
-// THIS TEST WAS GENERATED AUTOMATICALLY ON Tue May 03 2016 15:09:24 GMT+0300 (EEST)
+// THIS TEST WAS GENERATED AUTOMATICALLY ON Tue May 03 2016 15:31:04 GMT+0300 (EEST)
 
 'use strict';
 
 import chai from 'chai';
 import Handler from '../../node_modules/LambdaSizeRetrieve/Handler';
 
-let expect = chai.expect;
-
 // @todo: Add more advanced tests
 suite('Handlers', () => {
   test('Class Handler exists in lambda-size module', () => {
-    expect(Handler).to.be.an('function');
+    chai.expect(Handler).to.be.an('function');
   });
 });
